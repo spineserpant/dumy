dfc644f24d42b15345ce329a09ac142d37eda9f0
CSEL351 <sandeep504@sasi.ac.in>
2018-05-14 10:16:01 +0530

    this is demo

---
+++ app/src/main/java/com/example/csel351/dumy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.csel351.dumy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
